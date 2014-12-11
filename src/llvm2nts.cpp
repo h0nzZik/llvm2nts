851557657bd5ff8988d954ab6fdc48168ecc11b6
Jan Tusil <410062@mail.muni.cz>
2014-12-11 12:39:10 +0100

    AddInst: signed/unsigned overflow. Better consts.

--- src/llvm2nts.cpp
+++ src/llvm2nts.cpp
@@ -2,6 +2,7 @@
 #include "llvm/Pass.h"
 // Note: In version 3.5 ValueMap is inside IR directory
 #include "llvm/ADT/ValueMap.h"
+#include "llvm/ADT/APInt.h"
 #include "llvm/IR/Function.h"
 // Note: In version 3.5 InstIterator is inside IR directory
 #include "llvm/Support/InstIterator.h"
@@ -25,6 +26,11 @@
 using namespace llvm;
 using namespace NTS;
 
+// TODO: Support types
+// TODO: Memory leaks: store all formulas somewhere
+// TODO: at most two VariableIdentifiers for each variable
+// (to prevent allocating new and new again)
+
 // TODO: try argument promotion pass (-argpromotion)
 
 llvm2nts::llvm2nts(const llvm::Type *t)
@@ -32,7 +38,6 @@ llvm2nts::llvm2nts(const llvm::Type *t)
 	t->print(errs());
 	m_nts.addState(); // first (initial) state
 	m_return_type = t;
-	// TODO: Add variable of that type
 
 	NTS::Variable *v = new NTS::Variable("ret_var");
 	m_nts.setRetVar(v);
@@ -69,12 +74,12 @@ NTS::ArithmeticLiteral * llvm2nts::createArithmeticLiteral(const llvm::Value * v
 {
 	if (isa<Constant>(value))
 	{
-		// TODO: Somehow remove type from the output
-		// now there is: 'i32 1'
 		const auto *c = cast<llvm::ConstantInt>(value);
+		const auto &v = c->getValue();
 		auto * pn = new NTS::PositiveNumeral();
 		llvm::raw_string_ostream os(pn->getStr());
-		c->print(os);
+		v.print(os, false); // TODO: what to do with isSigned parameter?
+
 		return pn;
 	}
 
@@ -83,7 +88,6 @@ NTS::ArithmeticLiteral * llvm2nts::createArithmeticLiteral(const llvm::Value * v
 
 const NTS::Variable * llvm2nts::addValue(const Value *val)
 {
-	errs() << "Addvalue\n";
 	/* Construct the name of NTL variable.
 	 * Because in NTL there is no variable shadowing,
 	 * global and local variables have to have different names.
@@ -129,10 +133,8 @@ void llvm2nts::processInstruction(const Instruction &i)
 	errs() << "\n";
 
 
-	// FIXME: What if one of arguments is a constant?
-
 	// TODO: Ensure there is no problem with casting and saving values to our map
-	//
+	// TODO: Split this
 	switch(i.getOpcode())
 	{
 		case Instruction::Alloca:
@@ -189,25 +191,174 @@ void llvm2nts::processInstruction(const Instruction &i)
 
 		case Instruction::Add:
 			{
-				errs() << "Add\n";
 				auto &bo = cast<llvm::BinaryOperator>(i);
 				const llvm::Value *l = bo.getOperand(0);
 				const llvm::Value *r = bo.getOperand(1);
-				// TODO: Overflow
-				// 'no unsigned unwrap' and 'no signed unwrap' are not implemented
-				// see http://llvm.org/releases/3.5.0/docs/LangRef.html#add-instruction
-				// see http://llvm.org/releases/3.5.0/docs/LangRef.html#poisonvalues
 
-				Formula *f = new FormulaBop(BoolOp::And,
-						new AtomicRelation(AtomicRelation::Relation::Eq,
-							new ArithmeticVariableIdentifier(ret_value, true),
-							new ArithmeticTermRelation(ArithmeticTermRelation::Connector::Add,
-								createArithmeticLiteral(l),
-								createArithmeticLiteral(r)
-								)
-							),
-						new Havoc({ret_value})
+				auto * l_read = createArithmeticLiteral(l);
+				auto * r_read = createArithmeticLiteral(r);
+				auto * res_write = new ArithmeticVariableIdentifier(ret_value, true);
+
+				// Overflow handling
+				// LLVM uses 'no unsigned unwrap' and 'no signed unwrap' flags.
+				// For better understanding visit documentation:
+				// http://llvm.org/releases/3.5.0/docs/LangRef.html#add-instruction
+				// http://llvm.org/releases/3.5.0/docs/LangRef.html#poisonvalues
+
+				unsigned int bw = bo.getType()->getIntegerBitWidth();
+				// TODO: add support for greater scales (eg using APInt)
+				if (bw >= 8 * sizeof(unsigned long long int))
+					throw std::domain_error("Bit width too large");
+
+				unsigned long long int ub = 1ULL << bw;
+				unsigned long long int sb = 1ULL << (bw - 1);
+
+				auto * unsigned_bound = new PositiveNumeral();
+				{
+					llvm::raw_string_ostream os(unsigned_bound->getStr());
+					os << ub;
+					os.flush();
+				}
+
+				// a + b
+				auto * a_plus_b = new ArithmeticTermRelation(
+						ArithmeticTermRelation::Connector::Add,
+						l_read, r_read
 						);
+
+				// (a + b) % 256
+				auto * a_plus_b_mod = new ArithmeticTermRelation(
+						ArithmeticTermRelation::Connector::Mod,
+						a_plus_b,
+						unsigned_bound
+						);
+
+				// x' = (a + b) % 256
+				auto * f_assign = new AtomicRelation(AtomicRelation::Relation::Eq,
+						res_write,
+						a_plus_b_mod
+						);
+
+				Formula * f_nuw_pre = NULL;
+				if (bo.hasNoUnsignedWrap())
+				{
+					// Precondition for 'no unsigned unwrap'
+					f_nuw_pre = new AtomicRelation(AtomicRelation::Relation::Lt,
+							a_plus_b,
+							unsigned_bound
+							);
+				}
+
+				Formula * f_nsw_pre = NULL;
+				if (bo.hasNoSignedWrap())
+				{
+					auto *signed_bound = new PositiveNumeral();
+					{
+						llvm::raw_string_ostream os(signed_bound->getStr());
+						os << sb;
+						os.flush();
+					}
+
+					// Precondition for 'no signed unwrap':
+					// If both arguments are positive, result is positive, too.
+					// If both arguments are negative, result is negative, too.
+
+					// Atomic proposition for 'left operand is positive'
+					auto * l_positive = new AtomicRelation(
+							AtomicRelation::Relation::Lt,
+							l_read,
+							signed_bound
+							);
+
+					// 'lef operand is negative'
+					auto *l_negative = new AtomicRelation(
+							AtomicRelation::Relation::Ge,
+							r_read,
+							signed_bound
+							);
+
+					// 'right operand is positive'
+					auto *r_positive = new AtomicRelation(
+							AtomicRelation::Relation::Lt,
+							r_read,
+							signed_bound
+							);
+
+					// 'right operand is negative'
+					auto *r_negative = new AtomicRelation(
+							AtomicRelation::Relation::Ge,
+							r_read,
+							signed_bound
+							);
+
+					// 'result is positive'
+					auto *res_positive = new AtomicRelation(
+							AtomicRelation::Relation::Lt,
+							a_plus_b,
+							signed_bound
+							);
+
+					// 'result is negative'
+					auto *res_negative = new AtomicRelation(
+							AtomicRelation::Relation::Ge,
+							a_plus_b_mod,
+							signed_bound
+							);
+
+					auto *both_positive = new FormulaBop(
+							BoolOp::And,
+							l_positive,
+							r_positive
+							);
+
+					auto *both_negative = new FormulaBop(
+							BoolOp::And,
+							l_negative,
+							r_negative
+							);
+
+					auto *nsw_pre1 = new FormulaBop(
+							BoolOp::Impl,
+							both_positive,
+							res_positive
+							);
+
+					auto * nsw_pre2 = new FormulaBop(
+							BoolOp::Impl,
+							both_negative,
+							res_negative
+							);
+
+					f_nsw_pre = new FormulaBop(
+							BoolOp::And,
+							nsw_pre1,
+							nsw_pre2
+							);
+				}
+
+				Formula *pre = NULL;
+				if (f_nsw_pre || f_nuw_pre)
+				{
+					if (f_nsw_pre && !f_nuw_pre)
+					{
+						pre = f_nsw_pre;
+					} else if (!f_nsw_pre && f_nuw_pre) {
+						pre = f_nuw_pre;
+					} else {
+						pre = new FormulaBop(BoolOp::And, f_nsw_pre, f_nuw_pre);
+					}
+				}
+
+				Formula *f_add;
+				if (pre)
+				{
+					f_add = new FormulaBop(BoolOp::Impl, pre, f_assign);
+				} else {
+					f_add = f_assign;
+				}
+
+				auto *f_havoc = new Havoc({ret_value});
+				Formula *f = new FormulaBop(BoolOp::And, f_add, f_havoc);
 
 				const State & st_from = m_nts.lastState();
 				const State & st_to = m_nts.addState();
