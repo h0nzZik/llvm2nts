be8c98dbdba5ec4a1ac42782a5c9b77b2cab26e4
Jan Tusil <410062@mail.muni.cz>
2015-04-27 10:33:28 +0200

    Removed llvm pass, use 'run' instead.

--- run_grind.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-EXDIR="./externals/nts-examples"
-valgrind --track-origins=yes opt -load ./pass/libpass_llvm2nts.so -instnamer -llvm2nts "$EXDIR/02_gcd.ll" \
-	-llvm2nts_of="output.nts" > /dev/null
